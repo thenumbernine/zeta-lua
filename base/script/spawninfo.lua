c799df8652c2647675aa3b21c77b56dc60bb16a4
Christopher Moore <christopher.e.moore@gmail.com>
2016-05-07 18:43:43 +0300

    moar

---
+++ base/script/spawninfo.lua
@@ -0,0 +1,17 @@
+local class = require 'ext.class'
+local SpawnInfo = class()
+
+function SpawnInfo:init(args)
+	for k,v in pairs(args) do self[k] = v end
+end
+
+function SpawnInfo:respawn()
+	if not self.spawn then
+		error("failed to find spawn class")
+	end
+	-- self.spawn is a table, so self:spawn() is actually calling the table's ctor with self as the first param (after the table itself)
+	self.obj = self:spawn()
+	self.obj.spawnInfo = self
+end
+
+return SpawnInfo
