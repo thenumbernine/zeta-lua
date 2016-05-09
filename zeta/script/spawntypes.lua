afeb9ad3f4ac425c6d5dc27ea83e912cffa7a2a5
Christopher Moore <christopher.e.moore@gmail.com>
2016-05-10 01:58:46 +0300

    moar

---
+++ zeta/script/spawntypes.lua
@@ -0,0 +1,13 @@
+return {
+	{spawn='zeta.script.obj.blaster'},
+	{spawn='zeta.script.obj.missilelauncher'},
+	{spawn='zeta.script.obj.grenadelauncher'},
+	{spawn='zeta.script.obj.plasmarifle'},
+	{spawn='zeta.script.obj.heart'},
+	{spawn='zeta.script.obj.attackbonus'},
+	{spawn='zeta.script.obj.defensebonus'},
+	{spawn='zeta.script.obj.geemer'},
+	{spawn='zeta.script.obj.redgeemer'},
+	{spawn='zeta.script.obj.turret'},
+	{spawn='zeta.script.obj.gato'},
+}
