85e137880cf7039f86fcbb9f133c3cf116290ae5
Christopher Moore <christopher.e.moore@gmail.com>
2016-05-26 18:47:08 +0300

    oops

---
+++ zeta/script/tiletypes.lua
@@ -0,0 +1,8 @@
+return {
+	{name='blaster-break', solid=true},
+	{name='plasma-break', solid=true},
+	{name='skillsaw-break', solid=true},
+	{name='missile-break', solid=true},
+	{name='grenade-break', solid=true},
+	{name='speed-break', solid=true},
+}
