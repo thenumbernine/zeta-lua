74091360e7c48b3ad7636208c7e597c493d1d0fe
Christopher Moore <christopher.e.moore@gmail.com>
2023-07-24 20:37:37 +0700

    i guess i don't need gles-specific renderer anymore

--- base/script/singleton/class/renderer.lua
+++ base/script/singleton/class/renderer.lua
@@ -14,13 +14,6 @@ do
 	GLRenderer.glname = 'gl'
 	Renderer.requireClasses.gl = GLRenderer
 
-	local uvs = {
-		vec2(0,0),
-		vec2(1,0),
-		vec2(1,1),
-		vec2(0,1),
-	}
-
 	local vertexes = ffi.new('float[12]',
 		0, 0, 0,
 		1, 0, 0,
@@ -164,158 +157,12 @@ void main() {
 			gl.glUniform4f(shader.uniforms.defaultCenterAndRot.loc, rcx, rcy, costh, sinth)
 		end
 
-		gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, false, 12, vertexes)
+		gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, false, 0, vertexes)
 		gl.glEnableVertexAttribArray(0)
 		gl.glDrawElements(gl.GL_TRIANGLE_STRIP,4,gl.GL_UNSIGNED_SHORT,tristrip)
         gl.glDisableVertexAttribArray(0)
 
 		shader:useNone()
-	end
-end
-
--- TODO I can get rid of this once I replace all gl->gles2 content, then I can just use the gles2 library with the typical gl renderer
-do
-	local GLES2Renderer = class(Renderer)
-	GLES2Renderer.glname = 'OpenGLES2'
-	Renderer.requireClasses.OpenGLES2 = GLES2Renderer
-	local vertexes = ffi.new('float[12]',
-		0, 0, 0,
-		1, 0, 0,
-		0, 1, 0,
-		1, 1, 0
-	)
-	local tristrip = ffi.new('unsigned short[4]',
-		0,1,2,3
-	)
-	local color = ffi.new('float[4]')
-
-	local gl
-	local GLTex2D, GLProgram, GLMatrix4x4
-	local shader
-	local viewx, viewy = 0, 0
-	local mvmat, projmat, texmat
-	function GLES2Renderer:init(gl_)
-		self.gl = gl_
-		gl = gl_
-		gl.glPixelStorei(gl.GL_UNPACK_ALIGNMENT, 1)
-		GLMatrix4x4 = require 'gles2.matrix4'
-		GLProgram = require 'gles2.program'
-		GLTex2D = require 'gles2.tex2d'
-		local texsys = modio:require 'script.singleton.texsys'
-		texsys:setVars(gl, GLTex2D, {
-			minFilter = gl.GL_NEAREST, --gl.GL_LINEAR,
-			magFilter = gl.GL_NEAREST, --gl.GL_LINEAR,
-		})
-
-		texmat = GLMatrix4x4()
-		mvmat = GLMatrix4x4()
-		projmat = GLMatrix4x4()
-		pmvmat = GLMatrix4x4()
-		shader = GLProgram{
-			vertexCode = [[
-uniform mat4 vtxmat;
-uniform mat4 texmat;
-attribute vec4 pos;
-varying vec2 tc;
-void main() {
-	tc = (texmat * pos).xy;
-	gl_Position = vtxmat * pos;
-}
-]],
-			fragmentCode=[[
-precision mediump float;
-uniform vec4 color;
-uniform sampler2D tex;
-varying vec2 tc;
-void main() {
-	gl_FragColor = color * texture2D(tex, tc);
-}
-]],
-			attributes={'pos'},
-			uniforms={tex=0},
-		}
-	end
-	function GLES2Renderer:createTex2D(args)
-		return GLTex2D(args)
-	end
-	function GLES2Renderer:createShader(args)
-		local newargs = {}
-		for k,v in pairs(args) do newargs[k] = v end
-		newargs.vertexCode = '#define ANDROID\n'..newargs.vertexCode
-		newargs.fragmentCode = '#define ANDROID\n'..newargs.fragmentCode
-		return GLProgram(newargs)
-	end
-	function GLES2Renderer:ortho(a,b,c,d,e,f)
-		projmat:ortho(a,b,c,d,e,f)
-	end
-	function GLES2Renderer:viewPos(x,y)
-		viewx = x
-		viewy = y
-	end
-	local f4 = ffi.new('float[4]')
-	function GLES2Renderer:quad(
-		x,y,
-		w,h,
-		tx,ty,
-		tw,th,
-		angle,	-- TODO
-		r,g,b,a,
-		customShader,
-		uniforms
-	)
-		local shader = customShader or shader
-		-- rotate would go between the two...
-		texmat:translateMultScale(tx,ty,0,tw,th,1)
-		mvmat:translateMultScale(x-viewx,y-viewy,0,w,h,1)
-		pmvmat:mult(projmat,mvmat)
-		color[0] = r
-		color[1] = g
-		color[2] = b
-		color[3] = a
-		gl.glUseProgram(shader.id)
-		gl.glVertexAttribPointer(shader.attributes.pos, 3, gl.GL_FLOAT, false, 12, vertexes)
-		gl.glEnableVertexAttribArray(shader.attributes.pos)
-		gl.glUniform1i(shader.uniforms.tex.loc, 0);
-		gl.glUniform4fv(shader.uniforms.color.loc, 1, color)
-		gl.glUniformMatrix4fv(shader.uniforms.vtxmat.loc, 1, false, pmvmat.v)
-		gl.glUniformMatrix4fv(shader.uniforms.texmat.loc, 1, false, texmat.v)
- 		if uniforms then
-			for k,v in pairs(uniforms) do
-				local loc = shader.uniforms[k].loc
-				if not loc then
-					error('tried to set unknown uniform '..k)
-				end
-				if type(v) == 'number' then
-					gl.glUniform1f(loc, v)
-				elseif type(v) == 'table' then
-					if #v == 3 then
-						for i=0,2 do
-							f4[i] = v[i+1]
-						end
-						gl.glUniform3fv(loc, 1, f4)
-					elseif #v == 4 then
-						for i=0,3 do
-							f4[i] = v[i+1]
-						end
-						gl.glUniform4fv(loc, 1, f4)
-					else
-						error('uniform cant handle table of length '..#v)
-					end
-				else
-					error('cant handle uniform type '..type(v))
-				end
-			end
-		end
-		gl.glDrawElements(gl.GL_TRIANGLE_STRIP,4,gl.GL_UNSIGNED_SHORT,tristrip)
-        gl.glDisableVertexAttribArray(shader.attributes.pos)
-		gl.glUseProgram(0)
-	end
-	function GLES2Renderer:report(s)
-		local err = gl.glGetError()
-		if err == 0 then return end
-		if s then s = tostring(s)..': ' end
-		print(s..err)
-		print(debug.traceback())
 	end
 end
 
