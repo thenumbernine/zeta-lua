73302566a650559cbf6a7e47f9a07ba5a75c715e
Christopher Moore <christopher.e.moore@gmail.com>
2025-05-20 20:33:16 +0800

    meh keep it

--- neko/script/level.lua
+++ neko/script/level.lua
@@ -1,5 +1,5 @@
 local NekoLevel = require 'base.script.level':subbehavior(
-	--require 'base.script.behavior.level_temperature'	-- not using it yet and TODO how come in the wasm lua5.4+libffi version this returns a string?
+	require 'base.script.behavior.level_temperature'
 )
 
 -- only spawn the player ... and whatever's close to him
